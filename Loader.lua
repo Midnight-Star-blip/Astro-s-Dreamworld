fe1fc3b4ff857b24d0b53e347a0f0c788e21ebbf
Midnight-Star-blip <eduardo3840553@gmail.com>
2026-06-10 22:08:35 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,10 +3,42 @@
    -- Protected by Meh Astro's Dreamworld get out of here!
 
 
-local url = "https://raw.githubusercontent.com/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/AstroMain.lua"
-local success, code = pcall(game.HttpGet, game, url)
-if success then
-    loadstring(code)()
-else
-    warn("Failed to load Astro Script")
+local AstroTheBestDud = "htt";
+local DandyCantMakeThis = "ps://";
+local BoxtenAndCosmoForever = "raw.githubusercontent.com";
+local PebblesIsCute = "/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/";
+local GoobLovesYou = "AstroMain.lua";
+
+local function AstroSecretFunction()
+    local TishaLovesSprout = AstroTheBestDud..DandyCantMakeThis..BoxtenAndCosmoForever..PebblesIsCute..GoobLovesYou;
+    return game:HttpGet(TishaLovesSprout);
+end
+
+local CosmoAndBoxten = {};
+CosmoAndBoxten["Astro"] = game;
+CosmoAndBoxten["Dandy"] = CosmoAndBoxten["Astro"].HttpGet;
+CosmoAndBoxten["Vee"] = loadstring;
+CosmoAndBoxten["Scraps"] = pcall;
+
+local function GoobIsBest()
+    local SproutIsAdorable = AstroSecretFunction();
+    if SproutIsAdorable and #SproutIsAdorable > 2000 then
+        local GigiLovesYou = CosmoAndBoxten["Vee"](SproutIsAdorable);
+        if GigiLovesYou then
+            CosmoAndBoxten["Scraps"](GigiLovesYou);
+            return true;
+        end
+    end
+    return false;
+end
+
+local RodgerTheDetective = 0;
+for AstroLoop = 1, 6 do
+    RodgerTheDetective = RodgerTheDetective + 1;
+    if GoobIsBest() then break end;
+    task.wait(0.3 + (AstroLoop * 0.1));
+end
+
+if RodgerTheDetective < 3 then
+    warn("Astro the best dud :> failed to load... Dandy wins this time");
 end
