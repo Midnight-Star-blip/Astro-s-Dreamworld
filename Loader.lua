aff83539e57e942c86d18ee48d1f64d13a3d6620
Midnight-Star-blip <eduardo3840553@gmail.com>
2026-06-11 10:32:32 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,35 +2,24 @@ local AstroTheBestDud = "htt"
 local DandyCantMakeThis = "ps://"
 local BoxtenAndCosmoForever = "raw.githubusercontent.com"
 local PebblesIsCute = "/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/"
-local GoobLovesYou = "AstroPart"
+local GoobLovesYou = "AstroMain"
 
-local function AstroSecretFunction(part)
-    local TishaLovesSprout = AstroTheBestDud .. DandyCantMakeThis .. BoxtenAndCosmoForever .. PebblesIsCute .. GoobLovesYou .. part .. ".lua"
-    print("Loading :D " .. TishaLovesSprout)
-    return game:HttpGet(TishaLovesSprout)
-end
-
-local function GoobIsBest(part)
-    local success, code = pcall(AstroSecretFunction, part)
-    if success and code and #code > 500 then
-        loadstring(code)()
-        print("✅ " .. part .. " Loaded yay!")
-        return true
-    else
-        warn("❌ Error :c " .. part)
-        return false
-    end
+local function AstroSecretFunction()
+    local url = AstroTheBestDud .. DandyCantMakeThis .. BoxtenAndCosmoForever .. PebblesIsCute .. GoobLovesYou .. ".lua"
+    print("Loading: " .. url)
+    return game:HttpGet(url)
 end
 
 print("Astro Loading...")
 
-for i = 1, 4 do
-    print("Loading :D " .. i)
-    if GoobIsBest(tostring(i)) then
-        task.wait(0.6)
-    else
-        task.wait(0.8)
-    end
+local success, code = pcall(AstroSecretFunction)
+
+if success and code and #code > 10000 then
+    print("✅ Loaded yay!")
+    loadstring(code)()
+else
+    warn("❌ Error :c, Goddammit Dandy")
+    
 end
 
-print("Astro the best dud :> ")
+print("Astro the best dud :> Proceso terminado")
