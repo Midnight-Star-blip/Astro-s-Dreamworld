d0ae767f74cae0480b7f47476f1a40048974596d
Midnight-Star-blip <eduardo3840553@gmail.com>
2026-06-10 22:21:31 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,44 +1,45 @@
+local AstroTheBestDud = "htt"
+local DandyCantMakeThis = "ps://"
+local BoxtenAndCosmoForever = "raw.githubusercontent.com"
+local PebblesIsCute = "/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/"
+local GoobLovesYou = "AstroPart"
 
-   -- Astro the best dud :> | Dandy can't make this | Boxten and Cosmo forever :P
-   -- Protected by Meh Astro's Dreamworld get out of here!
-
-
-local AstroTheBestDud = "htt";
-local DandyCantMakeThis = "ps://";
-local BoxtenAndCosmoForever = "raw.githubusercontent.com";
-local PebblesIsCute = "/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/";
-local GoobLovesYou = "AstroMain.lua";
-
-local function AstroSecretFunction()
-    local TishaLovesSprout = AstroTheBestDud..DandyCantMakeThis..BoxtenAndCosmoForever..PebblesIsCute..GoobLovesYou;
-    return game:HttpGet(TishaLovesSprout);
+local function AstroSecretFunction(part)
+    local TishaLovesSprout = AstroTheBestDud .. DandyCantMakeThis .. BoxtenAndCosmoForever .. PebblesIsCute .. GoobLovesYou .. part .. ".lua"
+    return game:HttpGet(TishaLovesSprout)
 end
 
-local CosmoAndBoxten = {};
-CosmoAndBoxten["Astro"] = game;
-CosmoAndBoxten["Dandy"] = CosmoAndBoxten["Astro"].HttpGet;
-CosmoAndBoxten["Vee"] = loadstring;
-CosmoAndBoxten["Scraps"] = pcall;
+local CosmoAndBoxten = {}
+CosmoAndBoxten["Astro"] = game
+CosmoAndBoxten["Dandy"] = CosmoAndBoxten["Astro"].HttpGet
+CosmoAndBoxten["Vee"] = loadstring
+CosmoAndBoxten["Scraps"] = pcall
 
-local function GoobIsBest()
-    local SproutIsAdorable = AstroSecretFunction();
-    if SproutIsAdorable and #SproutIsAdorable > 2000 then
-        local GigiLovesYou = CosmoAndBoxten["Vee"](SproutIsAdorable);
+local function GoobIsBest(part)
+    local SproutIsAdorable = AstroSecretFunction(part)
+    if SproutIsAdorable and #SproutIsAdorable > 500 then
+        local GigiLovesYou = CosmoAndBoxten["Vee"](SproutIsAdorable)
         if GigiLovesYou then
-            CosmoAndBoxten["Scraps"](GigiLovesYou);
-            return true;
+            CosmoAndBoxten["Scraps"](GigiLovesYou)
+            return true
         end
     end
-    return false;
+    return false
 end
 
-local RodgerTheDetective = 0;
-for AstroLoop = 1, 6 do
-    RodgerTheDetective = RodgerTheDetective + 1;
-    if GoobIsBest() then break end;
-    task.wait(0.3 + (AstroLoop * 0.1));
+local RodgerTheDetective = 0
+local fullCode = ""
+
+for AstroLoop = 1, 3 do
+    RodgerTheDetective = RodgerTheDetective + 1
+    if GoobIsBest(tostring(AstroLoop)) then
+        -- part loaded  yayy
+    end
+    task.wait(0.2)
 end
 
-if RodgerTheDetective < 3 then
-    warn("Astro the best dud :> failed to load... Dandy wins this time");
+if RodgerTheDetective >= 2 then
+    print("Astro the best dud :> Loaded successfully!")
+else
+    warn("Dandy can't make this... Astro failed to load :(")
 end
