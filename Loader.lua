0a4bd0cbe3f13e362356bd8e074208db76d9b516
Midnight-Star-blip <eduardo3840553@gmail.com>
2026-06-10 21:35:17 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,44 @@
-local a="htt";local b="ps://";local c="raw.githubusercontent.com";local d="/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/";local e="AstroMain.lua";local f=a..b..c..d..e;local g=game:HttpGet;local h=loadstring;local i=pcall;local j=function()local k=g(f);if k and #k>1000 then local l=h(k);if l then l()end end end;i(j);task.wait(0.3);i(j)
+
+   -- Astro the best dud :> | Dandy can't make this | Boxten and Cosmo forever :P
+   -- Protected by Meh Astro's Dreamworld get out of here!
+
+
+local AstroTheBestDud = "htt";
+local DandyCantMakeThis = "ps://";
+local BoxtenAndCosmoForever = "raw.githubusercontent.com";
+local PebblesIsCute = "/Midnight-Star-blip/Astro-s-Dreamworld/refs/heads/main/";
+local GoobLovesYou = "AstroMain.lua";
+
+local function AstroSecretFunction()
+    local TishaLovesSprout = AstroTheBestDud..DandyCantMakeThis..BoxtenAndCosmoForever..PebblesIsCute..GoobLovesYou;
+    return game:HttpGet(TishaLovesSprout);
+end
+
+local CosmoAndBoxten = {};
+CosmoAndBoxten["Astro"] = game;
+CosmoAndBoxten["Dandy"] = CosmoAndBoxten["Astro"].HttpGet;
+CosmoAndBoxten["Vee"] = loadstring;
+CosmoAndBoxten["Scraps"] = pcall;
+
+local function GoobIsBest()
+    local SproutIsAdorable = AstroSecretFunction();
+    if SproutIsAdorable and #SproutIsAdorable > 2000 then
+        local GigiLovesYou = CosmoAndBoxten["Vee"](SproutIsAdorable);
+        if GigiLovesYou then
+            CosmoAndBoxten["Scraps"](GigiLovesYou);
+            return true;
+        end
+    end
+    return false;
+end
+
+local RodgerTheDetective = 0;
+for AstroLoop = 1, 6 do
+    RodgerTheDetective = RodgerTheDetective + 1;
+    if GoobIsBest() then break end;
+    task.wait(0.3 + (AstroLoop * 0.1));
+end
+
+if RodgerTheDetective < 3 then
+    warn("Astro the best dud :> failed to load... Dandy wins this time");
+end
